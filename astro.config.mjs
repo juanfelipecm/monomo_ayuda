aec5a9ac4264f5b68d56fc7a2c8023d9294e029a
Juan Felipe Castillo <juanfelipe.22@gmail.com>
2024-12-03 19:27:52 -0500

    Setup inicial

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,27 +1,41 @@
 import { defineConfig } from 'astro/config';
 import starlight from '@astrojs/starlight';
 
+import tailwind from '@astrojs/tailwind';
+
 // https://astro.build/config
 export default defineConfig({
-	integrations: [
-		starlight({
-			title: 'My Docs',
-			social: {
-				github: 'https://github.com/withastro/starlight',
-			},
-			sidebar: [
-				{
-					label: 'Guides',
-					items: [
-						// Each item here is one entry in the navigation menu.
-						{ label: 'Example Guide', slug: 'guides/example' },
-					],
-				},
-				{
-					label: 'Reference',
-					autogenerate: { directory: 'reference' },
-				},
-			],
-		}),
-	],
+    integrations: [starlight({
+        title: 'My Docs',
+        logo: {
+            src: './src/assets/Logo_Monomo_negro.png',
+        },
+        customCss: [
+            // Path to your Tailwind base styles:
+            './src/tailwind.css',
+          ],
+        social: {
+            github: 'https://github.com/withastro/starlight',
+        },
+        sidebar: [
+            {
+                label: 'Guides',
+                items: [
+                    // Each item here is one entry in the navigation menu.
+                    { label: 'Example Guide', slug: 'guides/example' },
+                ],
+            },
+            {
+                label: 'Reference',
+                autogenerate: { directory: 'reference' },
+            },
+            {
+                label: 'Modelos',
+                autogenerate: {directory: 'modelos'}
+            }
+        ],
+		}), tailwind({
+            // Disable the default base styles:
+            applyBaseStyles: false,
+          })],
 });

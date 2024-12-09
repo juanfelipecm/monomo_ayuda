8261347223abf169dc81a1499e33098dfbe8e6ad
Juan Felipe Castillo <juanfelipe.22@gmail.com>
2024-12-09 12:04:12 -0500

    Finish template styles

--- astro.config.mjs
+++ astro.config.mjs
@@ -7,38 +7,73 @@ import tailwind from '@astrojs/tailwind';
 export default defineConfig({
     site: 'https://ayuda.monomo.com.co',
     integrations: [starlight({
-        title: 'My Docs',
+        title: 'Ayuda',
+        description: 'Portal de ayuda de Monomo',
+        favicon: '.src/assets/faviconV2.png',
         defaultLocale: 'root',
         logo: {
             src: './src/assets/Logo_Monomo_negro.png',
+            replacesTitle: true
         },
         customCss: [
             // Path to your Tailwind base styles:
             './src/tailwind.css',
           ],
         social: {
-            github: 'https://github.com/withastro/starlight',
+            instagram: 'https://instagram.com/monomo_co',
         },
         sidebar: [
-            {
-                label: 'Guides',
-                items: [
+            // {
+                // label: 'Guides',
+                // items: [
                     // Each item here is one entry in the navigation menu.
-                    { label: 'Example Guide', slug: 'guides/example' },
-                ],
-            },
-            {
-                label: 'Reference',
-                autogenerate: { directory: 'reference' },
-            },
+                    // { label: 'Example Guide', slug: 'guides/example' },
+                // ],
+            // },
             {
                 label: 'Modelos',
-                autogenerate: {directory: 'modelos'}
+                items: [
+                    'modelos/modelos-intro',    
+                    {
+                        label: 'Primeros pasos',
+                        autogenerate: {directory: 'modelos/primeros-pasos'},
+                    },
+                    {
+                        label: 'Tipos de proyectos',
+                        autogenerate: {directory: 'modelos/tipos-de-proyectos'},
+                    },                                                                                                                        
+                    {
+                        label: 'Contratos y Pagos',
+                        autogenerate: {directory: 'modelos/contratos-y-pagos'},
+                    },
+                    {
+                        label: 'Mejores prácticas',
+                        autogenerate: {directory: 'modelos/mejores-practicas'},
+                    },
+                ]
             },
             {
                 label: 'Marcas',
-                autogenerate: {directory: 'marcas'}
-            }
+                items: [
+                    'marcas/marcas-intro',    
+                    {
+                        label: 'Primeros pasos',
+                        autogenerate: {directory: 'marcas/primeros-pasos'},
+                    },
+                    {
+                        label: 'Tipos de proyectos',
+                        autogenerate: {directory: 'marcas/tipos-de-proyectos'},
+                    },                                                                                                                        
+                    {
+                        label: 'Encuentra y contrata',
+                        autogenerate: {directory: 'marcas/enuentra-y-contrata'},
+                    },
+                    {
+                        label: 'Mejores prácticas',
+                        autogenerate: {directory: 'marcas/mejores-practicas'},
+                    },
+                ]
+            }            
         ],
 		}), tailwind({
             // Disable the default base styles:
---
+++ src/content/docs/modelos/modelos-intro.md
@@ -0,0 +1,6 @@
+---
+title: Ayuda para modelos
+description: A guide in my new Starlight docs site.
+---
+
+Encuentra a continuación toda la documentación para modelos, incluyendo cómo ser modelo no modelo, cómo ser creador de contenido, cómo completar cada paso del proceso.

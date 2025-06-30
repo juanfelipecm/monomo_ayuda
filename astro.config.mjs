52ecf6e34cac3ec26f89aea33f85d657c8c0c3e0
florcabr <florencia.c@monomo.com.co>
2025-06-30 18:28:52 -0300

    Update astro.config.mjs

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,72 +1,82 @@
-import { defineConfig } from 'astro/config';
+import { defineConfig } from 'astro/config';More actions
 import starlight from '@astrojs/starlight';
+
 import tailwind from '@astrojs/tailwind';
 
+// https://astro.build/config
 export default defineConfig({
-  site: 'https://ayuda.monomo.com.co',
-  integrations: [
-    starlight({
-      title: 'Ayuda',
-      description: 'Portal de ayuda de Monomo',
-      favicon: '/images/prueba_ca.png',
-      defaultLocale: 'root',
-      logo: {
-        src: './src/assets/Logo_Monomo_negro.png',
-        replacesTitle: true
-      },
-      customCss: ['./src/tailwind.css'],
-      social: {
-        instagram: 'https://instagram.com/monomo_co'
-      },
-      sidebar: [
-        {
-          label: 'Modelos',
-          items: [
-            'modelos/modelos-intro',
-            {
-              label: 'Primeros pasos',
-              autogenerate: { directory: 'modelos/primeros-pasos' }
-            },
-            {
-              label: 'Tipos de proyectos',
-              autogenerate: { directory: 'modelos/tipos-de-proyectos' }
-            },
-            {
-              label: 'Contratos y Pagos',
-              autogenerate: { directory: 'modelos/contratos-y-pagos' }
-            },
-            {
-              label: 'Mejores prácticas',
-              autogenerate: { directory: 'modelos/mejores-practicas' }
-            }
-          ]
+    site: 'https://ayuda.monomo.com.co',
+    integrations: [starlight({
+        title: 'Ayuda',
+        description: 'Portal de ayuda de Monomo',
+        favicon: '/images/prueba_ca.png',
+        defaultLocale: 'root',
+        logo: {
+            src: './src/assets/Logo_Monomo_negro.png',
+            replacesTitle: true
         },
-        {
-          label: 'Marcas',
-          items: [
-            'marcas/marcas-intro',
+        customCss: [
+            // Path to your Tailwind base styles:
+            './src/tailwind.css',
+          ],
+        social: {
+            instagram: 'https://instagram.com/monomo_co',
+        },
+        sidebar: [
+            // {
+                // label: 'Guides',
+                // items: [
+                    // Each item here is one entry in the navigation menu.
+                    // { label: 'Example Guide', slug: 'guides/example' },
+                // ],
+            // },
             {
-              label: 'Primeros pasos',
-              autogenerate: { directory: 'marcas/primeros-pasos' }
+                label: 'Modelos',
+                items: [
+                    'modelos/modelos-intro',    
+                    {
+                        label: 'Primeros pasos',
+                        autogenerate: {directory: 'modelos/primeros-pasos'},
+                    },
+                    {
+                        label: 'Tipos de proyectos',
+                        autogenerate: {directory: 'modelos/tipos-de-proyectos'},
+                    },                                                                                                                        
+                    {
+                        label: 'Contratos y Pagos',
+                        autogenerate: {directory: 'modelos/contratos-y-pagos'},
+                    },
+                    {
+                        label: 'Mejores prácticas',
+                        autogenerate: {directory: 'modelos/mejores-practicas'},
+                    },
+                ]
             },
             {
-              label: 'Tipos de proyectos',
-              autogenerate: { directory: 'marcas/tipos-de-proyectos' }
-            },
-            {
-              label: 'Encuentra y contrata',
-              autogenerate: { directory: 'marcas/encuentra-y-contrata' }
-            },
-            {
-              label: 'Mejores prácticas',
-              autogenerate: { directory: 'marcas/mejores-practicas' }
-            }
-          ]
-        }
-      ]
-    }),
-    tailwind({
-      applyBaseStyles: true
-    })
-  ]
+                label: 'Marcas',
+                items: [
+                    'marcas/marcas-intro',    
+                    {
+                        label: 'Primeros pasos',
+                        autogenerate: {directory: 'marcas/primeros-pasos'},
+                    },
+                    {
+                        label: 'Tipos de proyectos',
+                        autogenerate: {directory: 'marcas/tipos-de-proyectos'},
+                    },                                           
+                    {
+                        label: 'Encuentra y contrata',
+                        autogenerate: {directory: 'marcas/encuentra-y-contrata'},
+                    },
+                    {
+                        label: 'Mejores prácticas',
+                        autogenerate: {directory: 'marcas/mejores-practicas'},
+                    },
+                ]
+            }            
+        ],
+		}), tailwind({
+            // Disable the default base styles:
+            applyBaseStyles: true,
+          })],
 });
